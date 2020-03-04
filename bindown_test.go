bb164b63ec99764a6ec3f3038f1714cd5976fa1f
Will Roden <willabides@github.com>
2020-03-04 14:11:56 -0600

    add more tests for bindown.go

--- bindown_test.go
+++ bindown_test.go
@@ -1,10 +1,72 @@
 package bindown
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
+
+func TestConfigFile_Write(t *testing.T) {
+	config := Config{
+		Downloaders: map[string][]*Downloader{
+			"foo": {
+				{
+					OS:          "windows",
+					Arch:        "amd64",
+					URL:         "http://fake",
+					Checksum:    "deadbeef",
+					ArchivePath: "foo/foo.exe",
+					Link:        true,
+					BinName:     "foo.exe",
+				},
+			},
+		},
+	}
+	t.Run("json", func(t *testing.T) {
+		dir := tmpDir(t)
+		file := filepath.Join(dir, "bindown.json")
+		err := ioutil.WriteFile(file, []byte("overwrite me"), 0600)
+		require.NoError(t, err)
+		configFile := &ConfigFile{
+			format: formatJSON,
+			file:   file,
+			Config: config,
+		}
+		err = configFile.Write()
+		require.NoError(t, err)
+		got, err := ioutil.ReadFile(file)
+		require.NoError(t, err)
+		var gotConfig Config
+		err = json.Unmarshal(got, &gotConfig)
+		require.NoError(t, err)
+		assert.Equal(t, config, gotConfig)
+	})
+
+	t.Run("yaml", func(t *testing.T) {
+		dir := tmpDir(t)
+		file := filepath.Join(dir, "bindown.json")
+		err := ioutil.WriteFile(file, []byte("overwrite me"), 0600)
+		require.NoError(t, err)
+		configFile := &ConfigFile{
+			format: formatYAML,
+			file:   file,
+			Config: config,
+		}
+		err = configFile.Write()
+		require.NoError(t, err)
+		got, err := ioutil.ReadFile(file)
+		require.NoError(t, err)
+		var gotConfig Config
+		err = yaml.Unmarshal(got, &gotConfig)
+		require.NoError(t, err)
+		assert.Equal(t, config, gotConfig)
+	})
+}
 
 func TestLoadConfigFile(t *testing.T) {
 	t.Run("json1", func(t *testing.T) {
@@ -36,4 +98,35 @@ func TestLoadConfigFile(t *testing.T) {
 		assert.Equal(t, formatJSON, cfg.format)
 		assert.Equal(t, cfgPath, cfg.file)
 	})
+}
+
+func TestConfig_Downloader(t *testing.T) {
+	config := Config{
+		Downloaders: map[string][]*Downloader{
+			"foo": {
+				{
+					OS:          "windows",
+					Arch:        "amd64",
+					URL:         "http://fake/windows",
+					Checksum:    "deadbeef",
+					ArchivePath: "foo/foo.exe",
+					BinName:     "foo.exe",
+				},
+				{
+					OS:          "darwin",
+					Arch:        "amd64",
+					URL:         "http://fake/darwin",
+					Checksum:    "deadbeef",
+					ArchivePath: "foo/foo",
+				},
+			},
+		},
+	}
+
+	got := config.Downloader("foo", "windows", "amd64")
+	assert.Equal(t, "http://fake/windows", got.URL)
+	got = config.Downloader("foo", "Windows", "AMD64")
+	assert.Equal(t, "http://fake/windows", got.URL)
+	got = config.Downloader("foo", "linux", "amd64")
+	assert.Nil(t, got)
 }
