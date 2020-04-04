477f02c470e2596f42d4e66f2f1a0b27904ed557
Will Roden <willabides@github.com>
2020-04-04 16:34:23 -0500

    Test_logCloseErr

--- util_test.go
+++ util_test.go
@@ -1,9 +1,12 @@
 package bindown
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"hash/fnv"
+	"log"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -72,4 +75,27 @@ func Test_must(t *testing.T) {
 	require.Panics(t, func() {
 		must(assert.AnError)
 	})
+}
+
+func Test_logCloseErr(t *testing.T) {
+	oldLogWriter := log.Writer()
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	t.Cleanup(func() {
+		log.SetOutput(oldLogWriter)
+	})
+	logCloseErr(&dummyCloser{})
+	require.Empty(t, buf.String())
+	logCloseErr(&dummyCloser{
+		err: assert.AnError,
+	})
+	require.True(t, strings.Contains(buf.String(), assert.AnError.Error()))
+}
+
+type dummyCloser struct {
+	err error
+}
+
+func (d *dummyCloser) Close() error {
+	return d.err
 }
