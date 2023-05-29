9197c7c307bba3d3e1d3b83e850247e68233a926
Will Roden <will@roden.cc>
2023-05-29 17:00:51 -0500

    don't overwrite bindown.yml

--- internal/build-bootstrapper/build_test.go
+++ internal/build-bootstrapper/build_test.go
@@ -15,9 +15,13 @@ func TestBuild(t *testing.T) {
 	if runtime.GOOS == "windows" {
 		t.Skip("skipping on windows")
 	}
-	got, err := build("v3.15.5", filepath.FromSlash("../../"))
+	origConfig, err := os.ReadFile(filepath.FromSlash("../../bindown.yml"))
+	require.NoError(t, err)
+	got, err := build("v4.0.0", filepath.FromSlash("../../"))
 	require.NoError(t, err)
 	want, err := os.ReadFile(filepath.FromSlash("testdata/want.txt"))
 	require.NoError(t, err)
 	require.Empty(t, cmp.Diff(string(want), got))
+	err = os.WriteFile(filepath.FromSlash("../../bindown.yml"), origConfig, 0o644)
+	require.NoError(t, err)
 }
